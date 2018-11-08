a9e136a93ccca7f05c9e1f20f5d9bdada840fa8a
Simon TRUXILLO <struxill@e3r13p11.42.fr>
2018-11-08 17:45:44 +0100

    1st backup libft

---
+++ Libft_backup/ft_strdup.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: struxill <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/06 12:46:18 by struxill          #+#    #+#             */
+/*   Updated: 2018/11/08 16:42:06 by struxill         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *src)
+{
+	int		len;
+	char	*dest;
+	int		i;
+
+	len = ft_strlen(src);
+	dest = (char*)malloc(len * sizeof(char) + 1);
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
---
+++ Libft_backup/libft.h
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: struxill <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/08 15:09:53 by struxill          #+#    #+#             */
+/*   Updated: 2018/11/08 17:23:11 by struxill         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef __LIBFT_H
+# define __LIBFT_H
+# include <unistd.h>
+# include <stdlib.h>
+# include <stddef.h>
+
+/*
+ **STR FAMILY
+*/
+void	ft_putchar(char c);
+void	ft_putchar2(char c);
+void	ft_putstr(char const *s);
+void	ft_putstr2(char const *s);
+void	ft_putnbr(int n);
+int		ft_strcmp(const char *s1, const char *s2);
+char	*ft_strdup(const char *src);
+size_t	ft_strlen(const char *s);
+
+/*
+ **MEM FAMILY
+*/
+
+/*
+ **IS FAMILY
+*/
+
+/*
+ **CONVERSIONS
+*/
+
+//int		toupper(int c);
+//int		tolower(int c);
+
+/*
+ **ADDITIONNAL FUNCTIONS
+*/
+double	ft_sqrt(double x);
+
+#endif
