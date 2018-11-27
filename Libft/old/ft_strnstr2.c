5130645e12cd45472b40e92c33abc2fded9ae701
Simon TRUXILLO <struxill@e1r4p19.42.fr>
2018-11-27 23:50:16 +0100

    backup update

--- Libft/old/ft_strnstr2.c
+++ Libft/old/ft_strnstr2.c
@@ -6,45 +6,34 @@
 /*   By: struxill <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/23 20:26:01 by struxill          #+#    #+#             */
-/*   Updated: 2018/11/27 20:15:08 by struxill         ###   ########.fr       */
+/*   Updated: 2018/11/27 18:14:38 by struxill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	size_t	len_needle;
+	size_t		i;
+	size_t		j;
+	size_t		len_saved;
 
-	len_needle = ft_strlen(needle);
+	i = 0;
+	j = 0;
+	len_saved = len;
 	if (ft_strcmp(haystack, needle) == ft_strcmp(haystack, ""))
 		return ((char*)haystack);
-	while (len && len_needle <= len && *haystack != '\0')
+	while (needle[j] && haystack[i] && len)
 	{
-		if (*haystack == *needle)
-			if (ft_strncmp(haystack, needle, len_needle) == 0)
-//				if (ft_strlen(haystack) >= len_needle)
-					return ((char*)haystack);
-		haystack++;
-		len--;
+		while (haystack[i] != needle[j] && haystack[i] && i < len)
+			i++;
+		while (haystack[i] == needle[j] && haystack[i]
+			&& needle[j++] && i < len)
+			i++;
+		if (needle[j] == '\0')
+			return ((char*)&(haystack[i - j]));
+		j = 0;
+		i = ft_strlen(haystack) - --len_saved;
 	}
 	return (NULL);
 }
-/*
-int		main(void)
-{
-	char	buf[10];
-	char	*original;
-	char	*mine;
-
-	bzero(buf, 10);
-	strcpy(buf, "un deux 9");
-	buf[9] = '6';
-	buf[1] = 0;
-	mine = ft_strnstr(buf, "deux", 10);
-	original = strnstr(buf, "deux", 10);
-	printf("mine : %s\n", mine);
-	printf("original : %s\n", original);
-	return (0);
-}*/
--- Libft/old/ft_strnstr_old.c
+++ Libft/old/ft_strnstr_old.c
@@ -6,45 +6,38 @@
 /*   By: struxill <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/23 20:26:01 by struxill          #+#    #+#             */
-/*   Updated: 2018/11/27 20:15:08 by struxill         ###   ########.fr       */
+/*   Updated: 2018/11/23 20:31:32 by struxill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	size_t	len_needle;
+	size_t		i;
+	size_t		j;
+	const char	*ptr;
+	size_t		len_saved;
 
-	len_needle = ft_strlen(needle);
+	i = 0;
+	j = 0;
+	len_saved = len;
 	if (ft_strcmp(haystack, needle) == ft_strcmp(haystack, ""))
 		return ((char*)haystack);
-	while (len && len_needle <= len && *haystack != '\0')
+	while (needle[j] && haystack[i] && len)
 	{
-		if (*haystack == *needle)
-			if (ft_strncmp(haystack, needle, len_needle) == 0)
-//				if (ft_strlen(haystack) >= len_needle)
-					return ((char*)haystack);
-		haystack++;
-		len--;
+		while (haystack[i] != needle[j] && haystack[i] && len--)
+			i++;
+		while (haystack[i] == needle[j] && haystack[i] && needle[j++] && len--)
+			i++;
+		if (needle[j] == '\0')
+		{
+			ptr = &(haystack[i - j]);
+			return ((char*)ptr);
+		}
+		j = 0;
+		i = ft_strlen(haystack) - --len_saved;
+		len = len_saved;
 	}
 	return (NULL);
 }
-/*
-int		main(void)
-{
-	char	buf[10];
-	char	*original;
-	char	*mine;
-
-	bzero(buf, 10);
-	strcpy(buf, "un deux 9");
-	buf[9] = '6';
-	buf[1] = 0;
-	mine = ft_strnstr(buf, "deux", 10);
-	original = strnstr(buf, "deux", 10);
-	printf("mine : %s\n", mine);
-	printf("original : %s\n", original);
-	return (0);
-}*/
